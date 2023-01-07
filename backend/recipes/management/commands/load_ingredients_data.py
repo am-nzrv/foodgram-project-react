cc55ed6ab57c4888c8a4f98794027fd9dc3243c8
Армен Назаров <amnazaroff@gmail.com>
2023-01-07 20:40:57 +0500

    Initial commit

--- backend/recipes/management/commands/load_ingredients_data.py
+++ backend/recipes/management/commands/load_ingredients_data.py
@@ -2,14 +2,14 @@ from csv import DictReader
 
 from django.core.management import BaseCommand
 
-from recipes.models import Ingredients
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for row in DictReader(open('./data/ingredients.csv',
                                    encoding='utf-8')):
-            ingredients = Ingredients(
+            ingredients = Ingredient(
                 name=row['name'],
                 measurement_unit=row['measurement_unit']
             )

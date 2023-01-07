42ccb03eb214a9d217f9c1812279cf41cf446074
Армен Назаров <amnazaroff@gmail.com>
2023-01-07 21:22:40 +0500

    Initial commit

--- backend/recipes/management/commands/load_tags_data.py
+++ backend/recipes/management/commands/load_tags_data.py
@@ -6,12 +6,13 @@ from recipes.models import Tag
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         data = [
-            {'name': 'Завтрак', 'color': '#F61930', 'slug': 'breakfast'},
-            {'name': 'Обед', 'color': '#D8FF10', 'slug': 'dinner'},
-            {'name': 'Ужин', 'color': '#10B7FF', 'slug': 'supper'},
-            {'name': 'Первое', 'color': '#B840CF', 'slug': 'first'},
-            {'name': 'Второе', 'color': '#3DD25A', 'slug': 'second'},
-            {'name': 'Салат', 'color': '#003153', 'slug': 'salad'},
+            {'name': 'Завтрак', 'color': '#FF0000', 'slug': 'breakfast'},
+            {'name': 'Обед', 'color': '#C71585', 'slug': 'dinner'},
+            {'name': 'Ужин', 'color': '#006400', 'slug': 'supper'},
+            {'name': 'Первое', 'color': '#808000', 'slug': 'first'},
+            {'name': 'Второе', 'color': '#FFFF00', 'slug': 'second'},
+            {'name': 'Салат', 'color': '#000000', 'slug': 'salad'},
+            {'name': 'Десерт', 'color': '#00BFFF', 'slug': 'dessert'}
         ]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
         print('Теги успешно импортированы.')
